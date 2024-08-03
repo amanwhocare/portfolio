bff00ab45ea1e4ef404e9673dc2c5128e2f1dd43
Aman <aman.ramgharia36@gmail.com>
2024-08-03 12:41:05 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:2,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:3,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:4,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:5,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:6,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+
+    },
+   
+    {
+        id:7,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+    {
+        id:8,
+        name:"SEO Strategies for Developers: Boosting Your Site's Visibility",
+        tags:["SEO"],
+        date:"01 Aug, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/dQkAdUGCntA/upload/e52354659108cc57903ce348c0e5b821.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://amanwhocare.hashnode.dev/seo-strategies-for-developers-boosting-your-sites-visibility"
+    },
+  
+] 
